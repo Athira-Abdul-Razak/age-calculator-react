a3bf376cfa2c3a4ac6ca5846b5ff7c5d655f449e
Athira Abdul Razak <athiraabdulrazak@gmail.com>
2024-04-17 10:12:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AgeCalculator from './AgeCalculator.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
